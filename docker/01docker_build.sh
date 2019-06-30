deeea0d0e82a63befaa8f16d5511553a35c510df
ATTY Lionel <lionel.atty@gmail.com>
2019-06-30 19:52:05 +0200

    nvidia/cuda:9.2-devel-ubuntu18.04 -> nvidia/cuda:10.1-devel-ubuntu18.04

--- docker/01docker_build.sh
+++ docker/01docker_build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build -t darknet_python_server  .
+DOCKER_NAME=darknet_python_server
+DOCKER_TAG=nvidia-cuda-10.1-devel-ubuntu18.04
+
+docker build -t ${DOCKER_NAME}:${DOCKER_TAG}  .
